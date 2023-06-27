d2e7c5b81aee8b1d01ff11a9fb124e0905bcca5f
Tommy Verrall <60836166+tommyv1987@users.noreply.github.com>
2023-06-27 15:10:27 +0200

    Update validation.go

--- x/wasm/types/validation.go
+++ x/wasm/types/validation.go
@@ -16,7 +16,7 @@ var (
 	MaxLabelSize = 128 // extension point for chains to customize via compile flag.
 
 	// MaxWasmSize is the largest a compiled contract code can be when storing code on chain
-	MaxWasmSize = 800 * 1024 // extension point for chains to customize via compile flag.
+	MaxWasmSize = 1200 * 1024 // extension point for chains to customize via compile flag.
 
 	// MaxProposalWasmSize is the largest a gov proposal compiled contract code can be when storing code on chain
 	MaxProposalWasmSize = 3 * 1024 * 1024 // extension point for chains to customize via compile flag.
